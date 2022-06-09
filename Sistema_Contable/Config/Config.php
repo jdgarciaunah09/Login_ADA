7bd8270f614f9405c21a5783646f99f77d721ad8
Luisunah <93801481+Luisunah@users.noreply.github.com>
2022-06-08 23:35:10 -0600

    Add files via upload

---
+++ Sistema_Contable/Config/Config.php
@@ -0,0 +1,6 @@
+<?php
+  //define("BASE_URL", "http://localhost/Sistema-Contable/"):
+  const BASE_URL = "http://localhost/Sistema-Contable/";
+  const LIBS = "Libraries/";
+  const VIEWS = "Views/";
+?>
---
+++ Sistema_Contable/Controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home{
+    public function_construct()
+    {
+
+    }
+    
+    public function Home($parems)
+    {
+        echo "Mensaje desde el controlador";
+    }
+}
+?>
