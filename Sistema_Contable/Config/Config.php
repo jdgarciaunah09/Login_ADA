5c82427a05827e25489ce8f0c0fcedc9b39c9e90
Luisunah <93801481+Luisunah@users.noreply.github.com>
2022-06-08 23:12:22 -0600

    Delete Sistema_Contable directory

--- Sistema_Contable/Config/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-  //define("BASE_URL", "http://localhost/Sistema-Contable/"):
-  const BASE_URL = "http://localhost/Sistema-Contable/";
-  const LIBS = "Libraries/";
-  const VIEWS = "Views/";
-?>
